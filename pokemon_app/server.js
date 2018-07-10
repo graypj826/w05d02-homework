538cca1e565047a041c94d9e5e57a2c56a107881
Patrick Gray <patrickgray@Patricks-MacBook-Pro.local>
2018-07-10 16:51:36 -0600

    index.ejs view rendered at pokemon route

--- pokemon_app/server.js
+++ pokemon_app/server.js
@@ -10,11 +10,12 @@ const Pokemon = require("./models/pokemon")
 
 app.get("/", (req, res) => {
 	res.send("Welcome to the Pokemon App!")
-})
+});
+
 
 app.get("/pokemon", (req, res) => {
-	res.send(Pokemon)
-})
+	res.render("index.ejs")
+});
 
 
 
